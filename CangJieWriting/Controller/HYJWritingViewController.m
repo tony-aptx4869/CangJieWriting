996b9764353e7f65894df229a0134a5880db9287
WenFeng Zhang <wenfeng0218@icloud.com>
2015-09-04 10:54:52 +0800

    Create a new controller HYJWritingViewController

---
+++ CangJieWriting/Controller/HYJWritingViewController.m
@@ -0,0 +1,43 @@
+//
+//  HYJMainViewController.m
+//  CangJieWriting
+//
+//  Created by 张文峰 on 15/9/4.
+//  Copyright © 2015年 皓月君. All rights reserved.
+//
+
+#import "HYJWritingViewController.h"
+#import "HYJDrawingView.h"
+
+@interface HYJWritingViewController ()
+@property (weak, nonatomic) IBOutlet HYJDrawingView *writingView;
+
+@end
+
+@implementation HYJWritingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)setUpWritingView {
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- CangJieWriting/README.md
+++
@@ -1,6 +0,0 @@
-# CangJieWriting
-**仓颉书，一个汉字听写APP。**
-
-**CangJieWriting, A Chinese characters writing app.*
-
-Welcome to visit my Blog [http://www.haoyuejun.com](http://www.haoyuejun.com)
